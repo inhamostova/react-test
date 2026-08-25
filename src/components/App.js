c5a8392982ba177b8e006acbe46727d19a30766d
inhamostova <inha.mostova@gmail.com>
2026-08-25 13:29:31 +0100

    add todos app with form, list, but without styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,68 @@
-import { RecipeList } from './RecipeList';
+import { Component } from 'react';
+import { TodoList } from './TodoList/TodoList';
+import { TodoForm } from './TodoForm/TodoForm';
 
-import recipes from '../recipes.json';
+const initTodos = [
+  {
+    id: 'id-1',
+    task: 'Learn HTML',
+    checked: false,
+  },
+  {
+    id: 'id-2',
+    task: 'Learn CSS',
+    checked: false,
+  },
+  {
+    id: 'id-3',
+    task: 'Learn JS',
+    checked: false,
+  },
+];
 
-export const App = () => {
-  return (
-    <>
-      <RecipeList recipes={recipes} />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    todos: initTodos,
+  };
+
+  addTodo = task => {
+    this.setState(prevState => {
+      return {
+        todos: [
+          {
+            id: crypto.randomUUID(),
+            task,
+            checked: false,
+          },
+          ...prevState.todos,
+        ],
+      };
+    });
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(({ id }) => id !== todoId),
+    }));
+  };
+
+  checkTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo =>
+        todo.id === todoId ? { ...todo, checked: !todo.checked } : todo
+      ),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+    const { addTodo, deleteTodo, checkTodo } = this;
+
+    return (
+      <>
+        <TodoForm onSubmit={addTodo} />
+        <TodoList todos={todos} onDelete={deleteTodo} onCheck={checkTodo} />
+      </>
+    );
+  }
+}
